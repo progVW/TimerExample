8021ae726f38d4bbacf32623ff34d41a2f21f179
Sukharev Sergey <suharev.s@icloud.com>
2024-09-05 22:23:11 +0300

    Initial commit.

--- TimerExample/ViewController.swift
+++ TimerExample/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var count = 0
+    
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        printCountValue()
+        stopButton.isEnabled = false
     }
 
+    
+    func printCountValue() {
+        counterLabel.text = "Second passed: \(count)"
+    }
 
+    @IBAction func stopButtonAction(_ sender: Any) {
+        timer?.invalidate()
+        timer = nil
+        startButton.isEnabled = true
+        mySwitch.isEnabled = true
+        stopButton.isEnabled = false
+    }
+    
+    
+    @IBAction func startButtonAction(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countTime), userInfo: nil, repeats: true)
+        startButton.isEnabled = false
+        mySwitch.isEnabled = false
+        stopButton.isEnabled = true
+    }
+    
+    @objc func countTime() {
+        if mySwitch.isOn {
+            count += 1
+        } else {
+            count -= 1
+        }
+        printCountValue()
+    }
 }
 
